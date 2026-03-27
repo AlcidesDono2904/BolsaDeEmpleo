9cc5ad8327951c353a67bbb054d3ddddfc3181fe
Marisol Quirós <sol22qv@gmail.com>
2026-03-27 11:15:37 -0600

    Add functionality to view details

--- src/main/java/una/bolsadeempleo/controller/EmpresaController.java
+++ src/main/java/una/bolsadeempleo/controller/EmpresaController.java
@@ -2,6 +2,7 @@ package una.bolsadeempleo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import una.bolsadeempleo.logic.Service;
@@ -30,5 +31,32 @@ public class EmpresaController {
         service.guardarEmpresa(correo, password, nombre, localizacion, telefono, descripcion);
 
         return "redirect:/login";
+    }
+
+    //buscar candidato
+
+    /*@GetMapping("/candidatos/{idPuesto}")
+    public String buscarCandidatos(@PathVariable Integer idPuesto, Model model) {
+
+        var candidatos = service.buscarCandidatosParaPuesto(idPuesto);
+        var puesto = service.obtenerPuesto(idPuesto);
+
+        model.addAttribute("puesto", puesto);
+        model.addAttribute("candidatos", candidatos);
+
+        return "empresa/candidatos";
+    }*/
+
+    @GetMapping("/candidatos/{idPuesto}/detalle/{idOferente}")
+    public String verDetalle(@PathVariable Integer idPuesto,
+                             @PathVariable Integer idOferente,
+                             Model model) {
+
+        var oferente = service.obtenerOferente(idOferente);
+
+        model.addAttribute("oferente", oferente);
+        model.addAttribute("habilidades", oferente.getOferenteHabilidads());
+
+        return "empresa/detalle-candidato";
     }
 }
