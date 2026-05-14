d6448e97353fea4626792b1398058325e072db20
Marisol Quirós <sol22qv@gmail.com>
2026-05-14 12:13:30 -0600

    React Login fixed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,10 +12,9 @@ function Login() {
 
         try {
 
-            const response = await axios.post(
-                "http://localhost:8080/api/auth/login",
-                {
-                    correo,
+            const response =
+                await axios.post("http://localhost:8080/api/auth/login", {
+                    correo: correo,
                     passwordHash: clave
                 }
             );
